0a4cec3b03daf535c5910b1747f805c4ba8f6430
yuki <hmz1969a@gmail.com>
2024-10-15 15:43:48 +0800

    v0.0.6
    ε和β的排序问题被解决了. 在eta_func和beta_func的最初加入排序. 就可以使得运行时不再乱序
    于是修改了output, 现在output还会返回一个data.frame, 记录所有params的值, 并且也会针对ε和β进行排序

--- R/output.R
+++ R/output.R
@@ -29,8 +29,29 @@ output <- function(
   cat("AIC:", 2 * n_params - 2 * Log_Likelihood, "\n")
   cat("BIC:", n_params * log(n_trials) - 2 * Log_Likelihood, "\n", "\n")
   
+  # 在程序内对ε和β进行了排序, 但是在params输入时并没有排序
+  # output需要重新排序后输出
+  # 创建一个空的 data.frame
+  result_df <- data.frame(name = character(), value = numeric())
+  
+  # 将结果添加到 data.frame
   for (i in 1:ncol(ga_result@solution)) {
-    cat(params_name[i], ga_result@solution[1,i], "\n")
+    result_df <- rbind(result_df, data.frame(name = params_name[i], value = ga_result@solution[1,i]))
   }
   
+  # 筛选包含 "ε" 的行并排序
+  epsilon_rows <- grep("ε", result_df$name)
+  epsilon_values <- result_df[epsilon_rows, "value"]
+  epsilon_values_sorted <- sort(epsilon_values)
+  result_df[epsilon_rows, "value"] <- epsilon_values_sorted
+  
+  # 筛选包含 "β" 的行并排序
+  beta_rows <- grep("β", result_df$name)
+  beta_values <- result_df[beta_rows, "value"]
+  beta_values_sorted <- sort(beta_values)
+  result_df[beta_rows, "value"] <- beta_values_sorted
+  
+  rownames(result_df) <- NULL
+  # 查看结果
+  return(result_df)
 }
