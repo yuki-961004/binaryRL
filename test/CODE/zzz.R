cc55f7f0dbb0dc5168cbab1eb62d2dffad4d3d2d
yuki <hmz1969a@gmail.com>
2025-04-26 19:32:50 +0800

    tidy test
    整理下test文件夹, 东西太多了

---
+++ test/CODE/zzz.R
@@ -0,0 +1,43 @@
+utils::globalVariables(c(
+  "binaryRL.env",
+  "mode", "data", "id", "n_params", "n_trials"
+))
+
+binaryRLStartupMessage <- function()
+{
+  # Startup message obtained as 
+  # > figlet GA
+  msg <- c(paste0(
+  "
+                                      ╔══════════════════════════╗
+                                      ║ ╔----------╗             ║
+                                      ║ | ██████╗  |   ██╗       ║
+ |     _)                             ║ | ██╔══██╗ |   ██║       ║
+ __ \   |  __ \    _` |   __|  |   |  ║ | ██████╔╝ |   ██║       ║
+ |   |  |  |   |  (   |  |     |   |  ║ | ██╔══██╗ |   ██║       ║
+_.__/  _| _|  _| \__,_| _|    \__, |  ║ | ██║  ██║ |   ███████╗  ║
+                              ____/   ║ | ╚═╝  ╚═╝ |   ╚══════╝  ║
+                                      ║ ╚----------╝             ║
+                                      ╚══════════════════════════╝
+
+
+  "))
+  return(msg)
+}
+
+.onAttach <- function(lib, pkg)
+{
+  # startup message
+  if(!interactive()){
+    msg <- paste(
+      "Package 'binaryRL' Version: ", utils::packageVersion("binaryRL")
+    )
+  }
+  else {
+    msg <- binaryRLStartupMessage()
+  }
+  
+  packageStartupMessage(msg)
+  
+  invisible()
+}
