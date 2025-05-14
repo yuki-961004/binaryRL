bd476cda20d6531bdedac0130051e8432e71f396
yuki <hmz1969a@gmail.com>
2025-05-15 01:22:59 +0800

    try testthat

---
+++ tests/testthat.R
@@ -0,0 +1,3 @@
+library(testthat)
+library(binaryRL)
+testthat::test_check("binaryRL")
---
+++ tests/testthat/test-step1_run_m.R
@@ -0,0 +1,13 @@
+testthat::test_that("run_m() works with valid input", {
+  data <- binaryRL::Mason_2024_Exp1
+  
+  res <- binaryRL::run_m(
+    data = data,
+    id = 1,
+    eta = c(0.321, 0.765),
+    n_params = 2, 
+    n_trials = 360
+  )
+  
+  summary(res)
+})
