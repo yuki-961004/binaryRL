4aae543b8db2e93b644ea7c6504c495084b60ba7
yuki <110153830+yuki-961004@users.noreply.github.com>
2025-05-16 12:32:22 +0800

    v0.8.2
    再测一遍，改一下读csv的相对路径

--- tests/testthat/test-rev_e.R
+++ tests/testthat/test-rev_e.R
@@ -2,7 +2,7 @@
 testthat::test_that("rev_e() works with valid input", {
   review <- binaryRL::rev_e(
     data = binaryRL::Mason_2024_Exp2,
-    result = utils::read.csv("./demo//OUTPUT/result_comparison.csv"), 
+    result = utils::read.csv("../../demo//OUTPUT/result_comparison.csv"), 
     model = binaryRL::TD,
     model_name = "TD", 
     param_prefix = "param_",
@@ -16,7 +16,7 @@ testthat::test_that("rev_e() works with valid input", {
 testthat::test_that("rev_e() works with valid input", {
   review <- binaryRL::rev_e(
     data = binaryRL::Mason_2024_Exp2,
-    result = utils::read.csv("./demo//OUTPUT/result_comparison.csv"), 
+    result = utils::read.csv("../../demo//OUTPUT/result_comparison.csv"), 
     model = binaryRL::RSTD,
     model_name = "RSTD", 
     param_prefix = "param_",
@@ -30,7 +30,7 @@ testthat::test_that("rev_e() works with valid input", {
 testthat::test_that("rev_e() works with valid input", {
   review <- binaryRL::rev_e(
     data = binaryRL::Mason_2024_Exp2,
-    result = utils::read.csv("./demo//OUTPUT/result_comparison.csv"), 
+    result = utils::read.csv("../../demo//OUTPUT/result_comparison.csv"), 
     model = binaryRL::Utility,
     model_name = "Utility", 
     param_prefix = "param_",
