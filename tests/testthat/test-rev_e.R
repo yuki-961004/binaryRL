ac8247e07ab2799c218857b9046ce5894a22e2a2
yuki <hmz1969a@gmail.com>
2025-05-16 15:27:22 +0800

    v0.8.2
    使用filepath <- testthat::test_path("testdata", "result_comparison.csv")读取文件

--- tests/testthat/test-rev_e.R
+++ tests/testthat/test-rev_e.R
@@ -1,8 +1,11 @@
 # TD
 testthat::test_that("rev_e() works with valid input", {
+  filepath <- testthat::test_path("testdata", "result_comparison.csv")
+  comparison <- utils::read.csv(filepath)
+  
   review <- binaryRL::rev_e(
     data = binaryRL::Mason_2024_Exp2,
-    result = utils::read.csv("../../demo//OUTPUT/result_comparison.csv"), 
+    result = comparison, 
     model = binaryRL::TD,
     model_name = "TD", 
     param_prefix = "param_",
@@ -14,9 +17,12 @@ testthat::test_that("rev_e() works with valid input", {
 
 # RSTD
 testthat::test_that("rev_e() works with valid input", {
+  filepath <- testthat::test_path("testdata", "result_comparison.csv")
+  comparison <- utils::read.csv(filepath)
+  
   review <- binaryRL::rev_e(
     data = binaryRL::Mason_2024_Exp2,
-    result = utils::read.csv("../../demo//OUTPUT/result_comparison.csv"), 
+    result = comparison, 
     model = binaryRL::RSTD,
     model_name = "RSTD", 
     param_prefix = "param_",
@@ -28,9 +34,12 @@ testthat::test_that("rev_e() works with valid input", {
 
 # Utility
 testthat::test_that("rev_e() works with valid input", {
+  filepath <- testthat::test_path("testdata", "result_comparison.csv")
+  comparison <- utils::read.csv(filepath)
+  
   review <- binaryRL::rev_e(
     data = binaryRL::Mason_2024_Exp2,
-    result = utils::read.csv("../../demo//OUTPUT/result_comparison.csv"), 
+    result = comparison, 
     model = binaryRL::Utility,
     model_name = "Utility", 
     param_prefix = "param_",
